cbf9bd8665075e4539f4f1b182642f41055559e5
Animeshka39 <sergiosasa9@gmail.com>
2023-03-20 11:40:02 +0200

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CarouselBoxHk from "../CarouselBoxHk";
+
+class Home extends Component {
+    render() {
+        return (
+            <CarouselBoxHk />
+        );
+    }
+}
+
+export default Home;
